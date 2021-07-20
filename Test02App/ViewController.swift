1d1e2c1c7dc4b50c52b12a88f8bbd075f887f6ee
Ivan <ivnknv@zohomail.com>
2021-07-20 10:28:14 +0300

    Finished the interface work

--- Test02App/ViewController.swift
+++ Test02App/ViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed() {
+        if helloLabel.isHidden {
+            helloLabel.isHidden = false
+            startButton.setTitle("Clear text", for: .normal)
+        } else {
+            helloLabel.isHidden = true
+            startButton.setTitle("Show text", for: .normal)
+        }
+        
+    }
+    
 }
 
